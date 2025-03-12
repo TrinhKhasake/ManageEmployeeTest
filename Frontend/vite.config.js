e0ec680c9553f9757f859004ff46d02ddbe41d53
TrinhKhasake <10421023@student.vgu.edu.vn>
2025-03-12 11:02:03 +0700

    deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,21 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    // Optionally, increase the chunk size warning limit (default is 500 kB)
+    chunkSizeWarningLimit: 1000
+  }
 })
