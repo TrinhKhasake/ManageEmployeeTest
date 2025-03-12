773c0e0353b66bee6fcee0a0bfa5437c4cdbe3d3
TrinhKhasake <10421023@student.vgu.edu.vn>
2025-03-12 09:58:01 +0700

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -29,7 +29,8 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+const PORT = process.env.PORT || 3000;
 // Start the server
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
 });
